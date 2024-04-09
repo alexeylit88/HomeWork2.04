1422e55038e7a96a549c708ae7cca2b320d537ef
L.A <alexey.lit88@gmail.com>
2024-04-09 22:55:09 +0300

    Initial Commit

---
+++ HomeWork2.04/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork2.04
+//
+//  Created by ALEXEY LITVINOV on 09.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
